a6e5c2fd5723dd8aac14effa0eb9e6669e3cc90d
Petros Alexakis <apetros0303@gmail.com>
2026-06-03 19:01:26 +0300

    fix: update series IDs to currently active tournaments

--- config/settings.py
+++ config/settings.py
@@ -25,16 +25,11 @@ class Settings(BaseSettings):
 
     # Series IDs on CricAPI (update each season)
     CRICKET_SERIES: dict = {
-        # T20 leagues
-        "IPL": "87c62aac-bc3c-4738-ab93-19da0690488f",           # Indian Premier League 2026
-        "Big Bash": "4e2f50ed-ed84-46fc-bdcb-ace304b0da34",      # Big Bash League 2025-26
-        "CPL": "929c36f6-9ed6-4cec-a2ad-910a2ee4f701",           # Caribbean Premier League 2026
-        # International T20
-        "ICC T20 World Cup": "5978f057-af70-4dcf-b9ee-04831b8df947",   # ICC Men's T20 WC 2026
-        "ICC Womens T20 WC": "f3e5c7dd-332c-4893-9067-aa2bfe6d2b85",  # ICC Women's T20 WC 2026
-        # Test series
-        "ICC WTC Final": "e74332ae-5fee-4a43-814a-88ead6909e35",  # ICC WTC Final 2025
-        "The Ashes": "5d6b45ad-3699-4d15-84bc-4acb1dcf4ccd",     # The Ashes 2025-26
+        "India vs NZ": "9f0a2527-1508-42b3-9945-4ade13fd4b5a",       # India tour of New Zealand 2026
+        "ODI Qualifier": "7ac098a3-5d70-4c81-8e81-199edbeffc29",      # ICC WC League Two 2023-27
+        "ICC Womens T20 WC": "f3e5c7dd-332c-4893-9067-aa2bfe6d2b85", # ICC Women's T20 WC 2026
+        "CPL": "929c36f6-9ed6-4cec-a2ad-910a2ee4f701",                # Caribbean Premier League 2026
+        "IPL": "87c62aac-bc3c-4738-ab93-19da0690488f",                # Indian Premier League 2026
     }
 
     # Prediction engine weights
